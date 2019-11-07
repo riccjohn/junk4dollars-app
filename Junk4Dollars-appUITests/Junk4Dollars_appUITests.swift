5294fca79701a071e82d1a56b43d922dad83a3c8
riccjohn <riccjohn@gmail.com>
2019-11-07 11:15:54 -0600

    Remove launch performance test

--- Junk4Dollars-appUITests/Junk4Dollars_appUITests.swift
+++ Junk4Dollars-appUITests/Junk4Dollars_appUITests.swift
@@ -20,13 +20,4 @@ class Junk4Dollars_appUITests: XCTestCase {
         XCTAssertTrue(tablesQuery.staticTexts["Oko, Theif of Crowns"].exists)
         XCTAssertTrue(tablesQuery.staticTexts["MTG M20 Booster Box"].exists)
     }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
