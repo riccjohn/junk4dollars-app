588fda23aa607cdce04495e803dabeb42b4781fd
riccjohn <riccjohn@gmail.com>
2019-11-22 10:41:29 -0600

    Add tests for Auction data source

--- Junk4Dollars-appTests/ShowAuctions/AuctionListTableViewDataSourceTests.swift
+++ Junk4Dollars-appTests/ShowAuctions/AuctionListTableViewDataSourceTests.swift
@@ -2,10 +2,41 @@ import XCTest
 import UIKit
 import Junk4Dollars_app
 
-//class AuctionListTableViewDataSourceTests: XCTestCase {
-//    var dataSource: UITableViewDataSource!
-//
-//    func setupDataSource() {
-//        dataSource = AuctionListTableViewDataSource()
-//    }
-//}
+class AuctionListTableViewDataSourceTests: XCTestCase {
+    func testDataSource_SetsCorrectNumberOfCells() {
+        let dataSource = AuctionListTableViewDataSource()
+
+        let auctions = [
+            Auction(identifier: 1, title: "Title One"),
+            Auction(identifier: 2, title: "Title Two")
+        ]
+
+        let tableView = UITableView()
+
+        var numberOfRowsInSectionZero = dataSource.tableView(tableView, numberOfRowsInSection: 0)
+
+
+        XCTAssertEqual(0, numberOfRowsInSectionZero)
+
+        dataSource.setAuctions(auctions)
+        numberOfRowsInSectionZero = dataSource.tableView(tableView, numberOfRowsInSection: 0)
+
+        XCTAssertEqual(auctions.count, numberOfRowsInSectionZero)
+    }
+
+    func testDataSource_SetsTitleOfCellCorrectly() {
+        let dataSource = AuctionListTableViewDataSource()
+
+        let auctions = [
+            Auction(identifier: 1, title: "Title One"),
+            Auction(identifier: 2, title: "Title Two")
+        ]
+
+        dataSource.setAuctions(auctions)
+        let tableView = UITableView()
+        let indexPathZero = IndexPath(indexes: [0, 0])
+        let titleOfFirstCell = dataSource.tableView(tableView, cellForRowAt: indexPathZero)
+
+        XCTAssertEqual(auctions[0].title, titleOfFirstCell.textLabel?.text)
+    }
+}
