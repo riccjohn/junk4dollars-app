102514fd5212ba6833793043ccf2dfa2d7a5dc52
riccjohn <riccjohn@gmail.com>
2019-12-04 17:31:02 -0600

    Add tests for UserApiService

--- Junk4Dollars-appTests/API/UsersApiServiceTests.swift
+++ Junk4Dollars-appTests/API/UsersApiServiceTests.swift
@@ -29,4 +29,62 @@ class UsersApiServiceTests: XCTestCase {
         XCTAssertNotNil(actualUser)
         XCTAssertEqual("John", actualUser?.name)
     }
+
+    func testGetUserByToken_WhenApiClientReturns400WithNoData_TriggersCallbackWithErroro() {
+        let client = FakeApiClient()
+        let response = HTTPURLResponse(url: URL(string: "foo.com")!, statusCode: 400, httpVersion: nil, headerFields: [:])
+        let error: Error? = nil
+        client.stub(data: nil, response: response, error: error)
+        var isError = false
+
+        UserApiService(client: client).getUserByToken(token: "foo") { apiCallResult in
+            switch apiCallResult {
+                case .error:
+                    isError = true
+                default:
+                    break
+            }
+        }
+
+        XCTAssertTrue(isError)
+    }
+
+    func testGetUserByToken_WhenAPIClientReturns404WithInvalidJson_TriggersCallbackWithError() {
+        let client = FakeApiClient()
+        let data = "Not found".data(using: .utf8)
+        let response = HTTPURLResponse(url: URL(string: "foo.com")!, statusCode: 404, httpVersion: nil, headerFields: [:])
+        let error: Error? = nil
+        client.stub(data: data, response: response, error: error)
+        var isError = false
+
+        UserApiService(client: client).getUserByToken(token: "foo") { apiCallResult in
+            switch apiCallResult {
+                case .error:
+                    isError = true
+                default:
+                    break;
+            }
+        }
+
+        XCTAssertTrue(isError)
+    }
+
+    func testGetUserByToken_WhenApiClientReturnsValidJsonMissingField_TriggersCallbackWithError() {
+        let client = FakeApiClient()
+        client.stub(responseAsJson: [[
+            "id": 555
+        ]])
+        var isError = false
+
+        UserApiService(client: client).getUserByToken(token: "foo") { apiCallResult in
+            switch apiCallResult {
+                case .error:
+                    isError = true
+                default:
+                    break
+            }
+        }
+
+        XCTAssertTrue(isError)
+    }
 }
