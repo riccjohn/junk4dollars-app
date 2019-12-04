6f8817d9370e0219eae20e0af4ef47dba0df4c81
riccjohn <riccjohn@gmail.com>
2019-12-04 13:58:27 -0600

    Add actual Auth0 login/logout methods

--- Junk4Dollars-app/Authentication/Auth0Authentication.swift
+++ Junk4Dollars-app/Authentication/Auth0Authentication.swift
@@ -1,15 +1,38 @@
 import Auth0
 
 class Auth0Authentication: Authentication    {
-    var loggedIn = false
+    public var loggedIn = false
+    let credentialsManager = CredentialsManager(authentication: Auth0.authentication())
+    let APIIdentifier = "junk4dollars"
 
-    func logOut(_ callback: @escaping () -> Void) {
-        loggedIn = false
-        callback()
+    public func logOut(_ callback: @escaping () -> Void) {
+        Auth0
+        .webAuth()
+        .clearSession(federated:false) {
+            switch $0 {
+                case true:
+                    self.loggedIn = false
+                    callback()
+                case false:
+                   print("Error logging out")
+            }
+        }
     }
 
-    func logIn(_ callback: @escaping (_ token: String) -> Void) {
-        loggedIn = true
-        callback("foo")
+    public func logIn(_ callback: @escaping (_ token: String) -> Void) {
+        Auth0
+            .webAuth()
+            .scope("openid profile")
+            .audience(APIIdentifier)
+            .start {
+                switch $0 {
+                case .failure(let error):
+                    print("Error: \(error)")
+                case .success(let credentials):
+                    self.credentialsManager.store(credentials: credentials)
+                    self.loggedIn = true
+                    callback(credentials.accessToken!)
+                }
+        }
     }
 }
