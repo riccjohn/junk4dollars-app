ae4612d3de55b83447e23e480a16697a955383e3
riccjohn <riccjohn@gmail.com>
2019-12-09 13:53:39 -0600

    Refactor makeApiCall to utilize private methods

--- Junk4Dollars-app/API/AsyncApiClient.swift
+++ Junk4Dollars-app/API/AsyncApiClient.swift
@@ -2,29 +2,36 @@ import Foundation
 
 public class AsyncApiClient: ApiClient {
     public func makeApiCall(endpoint: String, authorized: Bool = false, whatToDoWithResponseData: @escaping (Data?, URLResponse?, Error?) -> Void) {
-        let url = URL(string: endpoint)!
-        var request: URLRequest = URLRequest(url: url)
-
         if(authorized) {
             AuthenticationDependencies.authentication.getAccessToken() { accessToken in
-                request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-                // inject URLSession.shared for testing
-                let task = URLSession.shared.dataTask(with: request) {data, response, error in
-                    if let error = error {
-                        print("ERROR: \(error)")
-                    }
-                    whatToDoWithResponseData(data, response, error)
-                }
+                let task = self.createURLSessionDataTask(endpoint: endpoint, accessToken: accessToken, callback: whatToDoWithResponseData)
+
                 task.resume()
             }
         } else {
-            let task = URLSession.shared.dataTask(with: request) {data, response, error in
-                if let error = error {
-                    print("ERROR: \(error)")
-                }
-                whatToDoWithResponseData(data, response, error)
-            }
+            let task = createURLSessionDataTask(endpoint: endpoint, accessToken: nil, callback: whatToDoWithResponseData)
             task.resume()
         }
+    }
+
+    private
+
+    func createURLSessionDataTask(endpoint: String, accessToken: String?, callback: @escaping(Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        let url = URL(string: endpoint)!
+        var request: URLRequest = URLRequest(url: url)
+
+        if let accessToken = accessToken {
+            request.addValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        }
+
+        // TODO: inject URLSession.shared for testing
+        let dataTask: URLSessionDataTask = URLSession.shared.dataTask(with: request) {data, response, error in
+            if let error = error {
+                print("ERROR: \(error)")
+            }
+            callback(data, response, error)
+        }
+
+        return dataTask
     }
 }
