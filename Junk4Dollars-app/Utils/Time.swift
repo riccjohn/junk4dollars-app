b52a9ac1184c456474f76a5201d7b3d63a9ef4ee
riccjohn <riccjohn@gmail.com>
2020-01-09 15:17:38 -0600

    Add static methods to Time struct

--- Junk4Dollars-app/Utils/Time.swift
+++ Junk4Dollars-app/Utils/Time.swift
@@ -24,4 +24,24 @@ public struct Time {
         let dateString = dateFormatter.string(from: date)
         return dateString
     }
+
+    public static func parseDateFrom(string: String) -> Date? {
+        let dateFormatter = DateFormatter()
+        let tempLocale = dateFormatter.locale
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+
+        guard let date = dateFormatter.date(from: string) else {
+            return nil
+        }
+
+        return date
+    }
+
+    public static func format(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy h:mm a"
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
 }
--- Junk4Dollars-appTests/Utils/TimeTests.swift
+++ Junk4Dollars-appTests/Utils/TimeTests.swift
@@ -2,6 +2,8 @@ import Junk4Dollars_app
 import XCTest
 
 class TimeTests: XCTestCase {
+
+    // will be outdated
     func test_localCanParseServerTime_toLocalFormat() {
         let serverTime: String = "2019-11-18T20:25:58.247Z"
         
@@ -9,8 +11,23 @@ class TimeTests: XCTestCase {
         XCTAssertEqual("11-18-2019 2:25 PM", localTime)
     }
 
+    // will be outdated
     func test_localReturnsNil_forInvalidDateFormat() {
         let serverTime: String = "2019-11-18T20:25:58Z"
         XCTAssertEqual(nil, Time(utc: serverTime).local())
     }
+
+    func test_parseDateFromString_returnsDate() {
+        let serverTime: String = "2019-11-18T20:25:58.247Z"
+        let date = Time.parseDateFrom(string: serverTime)
+        XCTAssertNotNil(date)
+        XCTAssertTrue(date! is Date)
+    }
+
+    func test_formatDate_returnsFormattedString() {
+        let date = Time.parseDateFrom(string: "2019-11-18T20:25:58.247Z")!
+        let dateString = Time.format(date: date)
+        XCTAssertEqual("11-18-2019 2:25 PM", dateString)
+    }
+
 }
