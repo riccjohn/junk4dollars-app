92b8848c71d88b0613858a0a40719253a8a54335
riccjohn <riccjohn@gmail.com>
2020-01-13 11:53:07 -0600

    Remove old method/tests for refactored code

--- Junk4Dollars-app/Utils/Time.swift
+++ Junk4Dollars-app/Utils/Time.swift
@@ -7,24 +7,6 @@ public struct Time {
         self.utc = utc
     }
 
-    public func local() -> String? {
-        let dateFormatter = DateFormatter()
-        let tempLocale = dateFormatter.locale
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-
-
-        guard let date = dateFormatter.date(from: self.utc) else {
-            return nil
-        }
-
-        dateFormatter.dateFormat = "MM-dd-yyyy h:mm a"
-        dateFormatter.locale = tempLocale
-
-        let dateString = dateFormatter.string(from: date)
-        return dateString
-    }
-
     public static func parseDateFrom(string: String) -> Date? {
         let dateFormatter = DateFormatter()
         let tempLocale = dateFormatter.locale
--- Junk4Dollars-appTests/Utils/TimeTests.swift
+++ Junk4Dollars-appTests/Utils/TimeTests.swift
@@ -2,21 +2,6 @@ import Junk4Dollars_app
 import XCTest
 
 class TimeTests: XCTestCase {
-
-    // will be outdated
-    func test_localCanParseServerTime_toLocalFormat() {
-        let serverTime: String = "2019-11-18T20:25:58.247Z"
-        
-        let localTime: String? = Time(utc: serverTime).local()
-        XCTAssertEqual("11-18-2019 2:25 PM", localTime)
-    }
-
-    // will be outdated
-    func test_localReturnsNil_forInvalidDateFormat() {
-        let serverTime: String = "2019-11-18T20:25:58Z"
-        XCTAssertEqual(nil, Time(utc: serverTime).local())
-    }
-
     func test_parseDateFromString_returnsDate() {
         let serverTime: String = "2019-11-18T20:25:58.247Z"
         let date = Time.parseDateFrom(string: serverTime)
